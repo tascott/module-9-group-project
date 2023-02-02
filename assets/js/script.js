83e7d6b7a0430c9b3eb93aaa476a77857eaeaa2a
jamellknows <jamellsamuels@googlemail.com>
2023-02-02 09:36:30 +0000

    push before swithcing branches:

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,14 @@
 console.log(news_api_key)
 let newsQueryUrl = `https://newsapi.org/v2/everything?q=keyword&apiKey=${news_api_key}`
 
-
+$('#search-button').click(function(){
+    let timeVal = $('#search-by-time').val()
+    let startVal = $('#search-by-destination-start').val()
+    let endVal = $('#search-by-destination-end').val()
+    console.log(timeVal)
+    console.log(startVal)
+    console.log(endVal)
+})
 
 $.ajax({
     url: newsQueryUrl,
@@ -10,11 +17,12 @@ $.ajax({
 }).then(function(response){
     let articles = response.articles
     $(articles).each(function(){
-        console.log($(this)[0].url)
+       // console.log($(this)[0].url)
         let url = $(this)[0].url
         $('#results').append(`<p>${url}</p>`)
     })
-    let website_to_scrape = articles[5].url.slice(0,-5)
+    let website_to_scrape = "https://www.nytimes.com/2020/09/02/opinion/remote-learning-coronavirus.html?action=click&module=Opinion&pgtype=Homepage"
+    articles[5].url.slice(0,-5)
     let scrapingUrl = `http://api.scraperapi.com?api_key=${scraping_api_key}&url=${website_to_scrape}`
     //console.log(website_to_scrape)
     
@@ -24,8 +32,7 @@ $.ajax({
 
     }).then(function(response){
         //console.log(response)
+        //console.log(scrap(response))
     })
     })
-
-
 
