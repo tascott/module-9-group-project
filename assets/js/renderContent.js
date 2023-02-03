808b6192b9dc0f3bc64b81350c474baae072a06a
TomScott <contact@tascott.co.uk>
2023-02-03 11:43:16 +0000

    Render example blog post in modal

--- assets/js/renderContent.js
+++ assets/js/renderContent.js
@@ -1,28 +1,66 @@
-// Get content from Medium API
-// Find a way to cache this for the user
+// Get content from Medium API and save to local storage
 
-// const options = {
-//     method: 'GET',
-//     headers: {
-//         'X-RapidAPI-Key': mediumAPIKey,
-//         'X-RapidAPI-Host': 'medium2.p.rapidapi.com'
-//     }
-// };
+const options = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': mediumAPIKey,
+        'X-RapidAPI-Host': 'medium2.p.rapidapi.com'
+    }
+};
 
 // fetch('https://medium2.p.rapidapi.com/topfeeds/technology/hot', options)
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
-// // Get an articles content
+// let examplePosts = [
+//             "2ae39662391b",
+//             "86e6818a050d",
+//             "3fb1cb3c2b5e",
+//             "55db62c1ae66",
+//             "f706555bf3d0",
+//             "32518b8143bd",
+//             "2656c726104f",
+//             "92a3ab9d39",
+//             "9f2a1cfab0e9",
+//             "4f3618876a2c",
+//             "a31a3d39b93e",
+//             "8e6c2746774d",
+//             "af9d33998240",
+//             "55bbe8379003",
+//             "40440c35cbf3",
+//             "476a67f53a59",
+//             "e846014e9fc0",
+//             "f7ca3ac3c28",
+//             "f26acc2a5841",
+//             "a7bf565db2a0",
+//             "7b38166b7708",
+//             "1bc57ad3dd07",
+//             "317c992c2a30",
+//             "6a36b5ab4d4a"
+// ];
+
+let examplePosts2 = [
+    "2ae39662391b",
+    "86e6818a050d",
+]
+
+// for each post, get the content
+// examplePosts2.forEach(function (post) {
+//     fetch(`https://medium2.p.rapidapi.com/article/${post}/content`, options)
+//         .then(response => response.json())
+//         .then(function (response) {
+//             localStorage.setItem(post, JSON.stringify(response));
+//         })
+//         .catch(err => console.error(err));
+// });
+
+// Get an articles content
 // fetch(`https://medium2.p.rapidapi.com/article/${articleID}/content`, options)
 //     .then(response => response.json())
 //     .then(response => console.log(response))
 //     .catch(err => console.error(err));
 
-// to be included in the data dump
-// x amount of links to news articles, including the article title, short description and link to the article - render card for each in article div
-//
 
 let newsDiv = $('#news-results');
 let videoDiv = $('#video-results');
@@ -107,6 +145,8 @@ function renderNews(data) {
     //render blogs
     html = '';
     data.blogs.forEach(function (blog) {
+        blog.content = JSON.parse(localStorage.getItem('86e6818a050d')).content;
+        console.log(blog.content);
         html += `
         <div class="card">
             <div class="card-body">
@@ -131,14 +171,20 @@ articleModal.addEventListener('show.bs.modal', function (event) {
     // Extract info from data-bs-* attributes
     var title = button.getAttribute('data-bs-title')
     var content = button.getAttribute('data-bs-content')
+    var author = button.getAttribute('data-bs-author')
+    var url = button.getAttribute('data-bs-url')
     // If necessary, you could initiate an AJAX request here
     // and then do the updating in a callback.
 
     var modalTitle = articleModal.querySelector('.modal-title')
     var modalBodyInput = articleModal.querySelector('.modal-text')
+    var modalUrl = articleModal.querySelector('.url')
+    var modalAuthor = articleModal.querySelector('.author')
 
+    modalUrl.attributes.href.value = url;
     modalTitle.textContent = title;
     modalBodyInput.textContent = content
+    modalAuthor.textContent = author
 })
 
 $(articleModal).on("hidden.bs.modal", function () {
